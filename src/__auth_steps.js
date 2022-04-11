0a1ae986309f3a10bbb549b6b6cafbd1cdf5feb6
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-11 13:48:59 +0600

    Implemented login signup

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -3,6 +3,12 @@
  * 2. npm install firebase
  * 3. Create firebase.init.js and import getAuth to export auth
  * 4. Firebase settings > Authentication > Enable Email and password authentication
+ * 5. Create Login, Signup Components, setup routes
+ * 6. Attach form field handler and form submit handler 
+ * 7. npm install --save react-firebase-hooks
+ * 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
+ * 9. if user is created redirect to the expected page
+ * 10. useSignInWithEmailAndPassword for sign in
  */
 
 
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,4 +1,4 @@
-import getAuth from 'firebase/auth';
+import {getAuth} from 'firebase/auth';
 
 
 // Import the functions you need from the SDKs you need
